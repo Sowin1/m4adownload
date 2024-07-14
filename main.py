775f8714497ab2e5bea927bcc3f5f25ce56538cf
Sowin Timéo <39094589+Sowin1@users.noreply.github.com>
2024-07-14 14:57:44 +0200

    update

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from yt_dlp import YoutubeDL
+from moviepy.editor import VideoFileClip
+
+def download_youtube_video_as_m4a(youtube_url, output_folder="result"):
+    # Vérifier si le dossier de sortie existe, sinon le créer
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Options de téléchargement
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(output_folder, '%(title)s.%(ext)s'),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+            'preferredquality': '192',
+        }],
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
+
+    print(f"Téléchargement terminé! Le fichier m4a est sauvegardé dans {output_folder}")
+
+# Exemple d'utilisation avec une URL YouTube valide
+download_youtube_video_as_m4a("https://youtu.be/RSl3TJItVjM?si=4nnd1hGOYSF2_Lg9")
